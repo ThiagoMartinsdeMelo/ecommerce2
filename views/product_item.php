0cb4f0852936774bd3d7fa870d9546f7cd7a667e
Thiago Martins <thiagompdi@gmail.com>
2017-08-15 07:44:05 -0300

    product home

--- views/product_item.php
+++ views/product_item.php
@@ -1,17 +1,25 @@
 <div class="product_item">
-	<div class="product_image">
-		<img src="<?php echo BASE_URL; ?>media/products/1.jpg"  width="100%"/>
-	</div>
-	<div class="product_name">
-		Monitor xxx
-	</div>
-	<div class="product_brand">
-		Sony
-	</div>
-	<div class="product_prince_from">
-		R$ XXX,XX
-	</div>
-	<div class="product_prince">
-		R$ XXX,XX
-	</div>	
+	<a href="">
+		<div class="product_tags">
+			<div class="product_tag product_tag_red">SALE</div>
+			<div class="product_tag product_tag_green">BEST SELLER</div>
+			<div class="product_tag product_tag_blue">NEW</div>
+		</div>
+		<div class="product_image">
+			<img src="<?php echo BASE_URL; ?>media/products/1.jpg"  width="100%"/>
+		</div>
+		<div class="product_name">
+			Monitor xxx
+		</div>
+		<div class="product_brand">
+			Sony
+		</div>
+		<div class="product_prince_from">
+			R$ XXX,XX
+		</div>
+		<div class="product_prince">
+			R$ XXX,XX
+		</div>
+		<div style="clear:both"></div>
+	</a>
 </div>
